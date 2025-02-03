3f5dfc6877e5ce35efbc9b87cfd7898b388c9eb5
sean-sanchez02 <sean.sanchez02@utrgv.edu>
2025-02-03 19:45:32 +0000

    not done

---
+++ src/Coverter.java
@@ -0,0 +1,23 @@
+public class Coverter {
+    public static void main (String[] args) {
+       // Convert the string strFloat into a float.
+        String strFloat = "374.25";
+        float from_strFloat; // assign here
+
+        // Convert the string strInt into an integer.
+        String strInt = "1250";
+        int from_strInt; // assign here
+
+        // Convert the float floatNum into an integer.
+        float floatNum = 56.715f;
+        int from_floatNum; // assign here
+
+        // Convert the integer intNum into a float.
+        int intNum = 1500;
+        float from_intNum; // assign here
+
+        // Convert the double doubleNum into a string.
+        double doubleNum = 3.14159265;
+        String from_doubleNum; // assign here 
+    }
+}
